91196b2e515df4a832549b62aa6c98dc3df1bab1
Gausulwindows <mohd64@amityonline.com>
2021-04-11 15:35:44 +0530

    First Mqtt Build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,25 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Observable } from 'rxjs';
+
+import { IMqttMessage,MqttModule,MqttService,IMqttServiceOptions} from 'ngx-mqtt';
+
+
+// define if or url in hostname 
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname:'[Host Name]',
+  port: 8083,
+  protocol: 'wss',
+  path: '/mqtt',
+};
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(),MqttModule.forRoot(MQTT_SERVICE_OPTIONS), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy,},MqttService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+import { IMqttMessage, MqttModule, MqttService } from 'ngx-mqtt';
+import { Observable,Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,45 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  sensor1:any;
+  constructor( private _mqttService: MqttService,public toastController: ToastController) {
+    
+    _mqttService.connect({username: '[username]', password: '[Password]'});
+  
+    this._mqttService.observe('[subscribe topic]').subscribe((message: IMqttMessage) => {  
+          this.sensor1 = message.payload.toString();
+          console.log(this.sensor1);
+          //console.log("System Connected");
+   },(e) => {
+    //  alert('Not Connected');
+    console.log("Not Connected");
+  });
+  this.reconnect();
+  }
 
-  constructor() {}
+
+  publishMessage()
+ {
+  let mqttjson:any='{"sendmsg":"ok"}';
+  this._mqttService.unsafePublish("publish topic", mqttjson, {qos: 0, retain: false});
+
+}
+
+
+//connect app on load
+ reconnect()
+ {
+   console.log('check connection');
+    this._mqttService.unsafePublish("connect", 'connect', {qos: 0, retain: false});
+    this._mqttService.observe('connect').subscribe((message: IMqttMessage) => {  
+      //check connection
+      console.log(message.payload.toString());
+      //console.log("System Connected");
+      },(e) => {
+      alert('Not Connected');
+      });
+ }
+
+
 
 }
